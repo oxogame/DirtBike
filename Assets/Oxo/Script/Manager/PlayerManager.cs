36f23ae3ad87ebb68aa789f910a848d446fd0f14
oxogames <62541774+oxogames@users.noreply.github.com>
2021-03-08 20:45:46 +0300

    meta

--- Assets/Oxo/Script/Manager/PlayerManager.cs
+++ Assets/Oxo/Script/Manager/PlayerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -14,6 +15,12 @@ public class PlayerManager : MonoBehaviour
 
     bool isMouseDown;
 
+    float minAngle = -20f;
+    float angle;
+
+    public List<GameObject> rockList = new List<GameObject>();
+    public GameObject emptySlot;
+
     private void Awake()
     {
         if (instance == null)
@@ -25,8 +32,7 @@ public class PlayerManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    float minAngle = -20f;
-    float angle;
+
     void FixedUpdate()
     {
         if (Input.GetMouseButtonDown(0))
@@ -43,10 +49,12 @@ public class PlayerManager : MonoBehaviour
 
                 var emision = mud.emission;
                 emision.rateOverTime = Mathf.Abs(angle * 50);
+                FillRock();
             }
             else
             {
                 bike.transform.eulerAngles = new Vector3(minAngle, 0f, 0f);
+                FillRock();
             }
 
             //bike.GetComponent<BikeController>().EngineTorque = 350f;
@@ -73,5 +81,16 @@ public class PlayerManager : MonoBehaviour
             emision.rateOverTime = Mathf.Abs(0f);
             //mudParticle.SetActive(false);
         }
+    }
+
+
+    void FillRock()
+    {
+        //emptySlot.transform.position = Vector3.Lerp(emptySlot.transform.position, new Vector3(emptySlot.transform.position.x, 0f, emptySlot.transform.position.z),0.02f);
+        GameObject go = Instantiate(rockList.FirstOrDefault().gameObject);
+        go.transform.position = bike.transform.forward * 25f;
+        go.transform.localScale = Vector3.one * Random.Range(0.7f, 0.9f);
+        go.SetActive(true);
+        go.transform.DOShakePosition(0.1f, 1f);
     }
 }
